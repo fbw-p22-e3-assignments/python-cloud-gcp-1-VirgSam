fa349d8ecc91cd6797a2129774d59f7eff7a38ce
chris01 <chris_odum@yahoo.com>
2023-05-31 09:30:24 +0200

    Upload blobfile

--- building_apis/first_api/storage_bucket_test.py
+++ building_apis/first_api/storage_bucket_test.py
@@ -6,4 +6,7 @@ client = storage.Client()
 new_bucket = client.create_bucket("hello-world-2-storage")
 
 # create a blob(file) to be uploaded
-new_blob= new_bucket.blob("test-folder")
+new_blob= new_bucket.blob("test-folder/manage.py")
+
+# upload the file
+new_blob.upload_from_filename(filename="first_api/manage.py")
