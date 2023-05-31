76b4cbf3247c088cf3b44b3ba0ab1845c908023d
chris01 <chris_odum@yahoo.com>
2023-05-31 09:26:42 +0200

    Create blobfile for upload

--- building_apis/first_api/storage_bucket_test.py
+++ building_apis/first_api/storage_bucket_test.py
@@ -5,4 +5,5 @@ client = storage.Client()
 # Create a new storage bucket
 new_bucket = client.create_bucket("hello-world-2-storage")
 
-# create
+# create a blob(file) to be uploaded
+new_blob= new_bucket.blob("test-folder")
